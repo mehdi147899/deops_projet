f43dcc3062fd0dd6d91b65a6c152fbceeefce014
mehdi467 <mehdi104072000@gmail.com>
2024-10-21 22:08:38 +0200

    a

--- src/main/java/tn/esprit/spring/entities/Course.java
+++ src/main/java/tn/esprit/spring/entities/Course.java
@@ -1,23 +1,23 @@
 package tn.esprit.spring.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
 import java.util.Set;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level=AccessLevel.PRIVATE)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class Course implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long numCourse;
 	int level;
 	@Enumerated(EnumType.STRING)
@@ -28,7 +28,6 @@ public class Course implements Serializable {
 	int timeSlot;
 
 	@JsonIgnore
-	@OneToMany(mappedBy= "course")
-	Set<Registration> registrations;
-
+	@OneToMany(mappedBy = "course")
+	transient Set<Registration> registrations; // Marked as transient // No need to change this if Registration is Serializable
 }
