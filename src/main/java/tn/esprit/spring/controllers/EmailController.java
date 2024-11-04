16f75957705fb17a71e03e1a6cfe1fc754361916
mehdi467 <mehdi104072000@gmail.com>
2024-11-04 20:02:32 +0100

    96.3

--- src/main/java/tn/esprit/spring/controllers/EmailController.java
+++ src/main/java/tn/esprit/spring/controllers/EmailController.java
@@ -1,23 +1,28 @@
 package tn.esprit.spring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tn.esprit.spring.services.EmailService;
 
 @RestController
+@RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
 
-    // Constructor injection
     public EmailController(EmailService emailService) {
         this.emailService = emailService;
     }
 
     @GetMapping("/sendEmail")
     public String sendEmail(@RequestParam String recipient) {
-        emailService.sendPipelineAlert("Pipeline Completed", "Your pipeline process has finished successfully.", recipient);
+        emailService.sendPipelineAlert(
+                "Pipeline Completed",
+                "Your pipeline process has finished successfully.",
+                recipient
+        );
         return "Email sent to " + recipient;
     }
 }
