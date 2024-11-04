0d37c4a11e879487aeb1bd59a993bb70616a7474
bilel910 github <kortbilel880@gmail.com>
2024-11-04 16:14:32 +0100

    last but not least 2.0

--- src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/PisteServicesImpl.java
@@ -8,17 +8,9 @@ import tn.esprit.spring.repositories.IPisteRepository;
 import java.util.List;
 @AllArgsConstructor
 @Service
-public class PisteServicesImpl implements IPisteServices {
+public class PisteServicesImpl implements  IPisteServices{
 
-    private final IPisteRepository pisteRepository;
-
-    @Override
-    public Piste addPiste(Piste piste) {
-        if (piste == null) {
-            throw new IllegalArgumentException("Piste cannot be null");
-        }
-        return pisteRepository.save(piste);
-    }
+    private IPisteRepository pisteRepository;
 
     @Override
     public List<Piste> retrieveAllPistes() {
@@ -26,18 +18,17 @@ public class PisteServicesImpl implements IPisteServices {
     }
 
     @Override
+    public Piste addPiste(Piste piste) {
+        return pisteRepository.save(piste);
+    }
+
+    @Override
     public void removePiste(Long numPiste) {
-        if (numPiste == null) {
-            throw new IllegalArgumentException("Piste ID cannot be null");
-        }
         pisteRepository.deleteById(numPiste);
     }
 
     @Override
     public Piste retrievePiste(Long numPiste) {
-        if (numPiste == null) {
-            throw new IllegalArgumentException("Piste ID cannot be null");
-        }
         return pisteRepository.findById(numPiste).orElse(null);
     }
 }
